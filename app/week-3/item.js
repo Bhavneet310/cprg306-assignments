44c4f2931e081083d5c09ecf6897c9be2e4d23f7
Bhavneet310 <152673309+Bhavneet310@users.noreply.github.com>
2024-10-01 11:49:28 -0600

    Update item.js

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,1 +1,13 @@
 
+export default function Item({ name, quantity, category }) {
+  return (
+    <li className="flex justify-between items-center p-4 border-b border-gray-200">
+      <div>
+        <span className="font-semibold text-lg">{name}</span> 
+        <span className="block text-gray-500 text-sm">{category}</span>
+      </div>
+      <div className="text-gray-700 font-medium">Qty: {quantity}</div>
+    </li>
+  );
+}
+
