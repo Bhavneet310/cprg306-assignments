ef67855a2229775ac9c63d9f41074fc322862b4a
Bhavneet310 <152673309+Bhavneet310@users.noreply.github.com>
2024-10-23 20:56:23 -0600

    Update page.js

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,22 +1,21 @@
 "use client"; 
 import React, { useState } from 'react';
-import ItemList from './ItemList';
-import AddNewItem from './AddNewItem';
-import initialItems from './items.json';
-const ShoppingPage = () => {
-  const [items, setItems] = useState(initialItems);
-  const addItem = (item) => {
-    setItems((previousItems) => [...previousItems, item]);
+import ItemList from './item-list';
+import NewItem from './new-item'; 
+import itemsData from './items.json';
+const Page = () => {
+  const [items, setItems] = useState(itemsData);
+  const handleAddItem = (newItem) => {
+    setItems((prevItems) => [...prevItems, newItem]);
   };
   return (
-    <main className="min-h-screen bg-gray-100 p-4">
-      <div className="container mx-auto max-w-xl">
-        <h1 className="text-3xl font-bold text-center text-gray-900 mb-6">Shopping List</h1>
-        <AddNewItem onItemAdd={addItem} />
+    <main className="min-h-screen bg-black-20 p-3">
+      <div className="max-w-xl mx-auto">
+        <h1 className="text-3xl font-bold text-center text-white-700 mb-6">Shopping List</h1>
+        <NewItem onAddItem={handleAddItem} />
         <ItemList items={items} />
       </div>
     </main>
   );
 };
-export default ShoppingPage;
-
+export default Page;
